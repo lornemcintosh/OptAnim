4ee68583b0a1587cdff58bf171992cb563c0b673
Lorne McIntosh <lorne@lorneswork.com>
2013-05-22 20:41:31 -0700

    Fixed mixed tab/space indentations in setup.py Removed email address from setup.py to prevent spam

--- setup.py
+++ setup.py
@@ -3,28 +3,27 @@
 from setuptools import setup
 
 setup(
-    name = 'optanim',
+	name = 'optanim',
 	version = '0.1',
-    description = 'A library for the procedural generation of 3D skeletal character animations',
+	description = 'A library for the procedural generation of 3D skeletal character animations',
 	license = 'GPL',
-    keywords = ['graphics', 'procedural', 'generation', '3D', 'skeletal', 'character', 'animation', 'videogame', 'optimal', 'spacetime', 'constraints', 'control', 'non-linear', 'optimization', 'parameter', 'space', 'physics'],
-    author = 'Lorne McIntosh',
-    author_email = 'lorne@lorneswork.com',
+	keywords = ['graphics', 'procedural', 'generation', '3D', 'skeletal', 'character', 'animation', 'videogame', 'optimal', 'spacetime', 'constraints', 'control', 'non-linear', 'optimization', 'parameter', 'space', 'physics'],
+	author = 'Lorne McIntosh',
 	url = 'https://github.com/lornemcintosh/optanim',
-    #package_dir = {'': 'src'},
+	#package_dir = {'': 'src'},
 	packages = ['optanim'],
-    #test_suite = 'your.module.tests',
+	#test_suite = 'your.module.tests',
 	classifiers = [
-        "Programming Language :: Python",
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
+		"Programming Language :: Python",
+		"Development Status :: 2 - Pre-Alpha",
+		"Environment :: Console",
+		"Intended Audience :: Developers",
 		"Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development :: Libraries :: Python Modules",
 		"Topic :: Scientific/Engineering :: Physics",
 		"Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Multimedia :: Graphics",
-        ]
+		"Topic :: Multimedia :: Graphics",
+	]
 )
